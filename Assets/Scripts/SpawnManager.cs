ca16ab42a8fc3eded44ff6c1e109dd5c73a06276
flexusISF <146860748+flexusISF@users.noreply.github.com>
2023-11-30 10:07:49 -0800

    DAY04
    got the player controller script to work with civic

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveForward : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
-
-    public float speed = 40.0f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +13,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
         
     }
 }
